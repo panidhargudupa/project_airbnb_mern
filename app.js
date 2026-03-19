992e435207523ca882b7e844ac8efbe0424f0f2b
PanidharGUdupa <panidharu@gmail.com>
2026-03-19 22:45:02 +0530

    done navbar,footer,new-page,edit-page,show-page updations

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 //CONNECTING WITH MOONGOOSE
 const mongo_url = "mongodb://127.0.0.1:27017/airbnb";
@@ -17,6 +18,8 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,'/public')));
 
 main()
     .then(() => {
